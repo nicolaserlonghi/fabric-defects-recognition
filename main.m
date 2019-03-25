de2c34478a8ee3ffc562641e023860be074ec2ac
Nicola Serlonghi <nicolaserlonghi@gmail.com>
2019-03-25 13:53:19 +0100

    [REFACTOR] code refactoring

--- main.m
+++ main.m
@@ -3,51 +3,38 @@
 % Ramon Elena
 % Serlonghi Nicola
 % Tonini Francesco
-
-% Experimental parameters
-% 1 :
-%       width = 20
-%       thresh = 0.85
-%       mask = 0.1
-%       disk = 2
-% 2 : 
-%       mask = 0.65
-% 4 : 
-%       width = 20
-%       thresh = 0.85
-%       mask = 0.2
-%       disk = 1
-
 clear all;
 close all;
 clc;
 
 [image, yImageSize, xImageSize] = loadImage("12.jpg");
 
-% Constants
+% Parametri da cambiare: pattern
 startPatternX = 1;
 startPatternY = 1;
 patternWidth = 20;
 threshold = 85;
-cut = 0.064;
+maskValue = 0.064;
+diskSize = 2;
 
 [pattern1, pattern2, pattern3, pattern4] = getPatterns(image, startPatternX, startPatternY, patternWidth, threshold);
 
-c1 = normxcorr2(pattern1, image);
-c2 = normxcorr2(pattern2, image);
-c3 = normxcorr2(pattern3,image);
-c4 = normxcorr2(pattern4,image);
 
-c = (c1 + c2 + c3 + c4) / 4;
-c = c(patternWidth : end - patternWidth, patternWidth : end - patternWidth);
-c=abs(c);
+normxcorrImage1 = normxcorr2(pattern1, image);
+normxcorrImage2 = normxcorr2(pattern2, image);
+normxcorrImage3 = normxcorr2(pattern3,image);
+normxcorrImage4 = normxcorr2(pattern4,image);
 
-mask = c < cut;
-se = strel('disk', 2);
-mask2 = imopen(mask, se);
-figure, imagesc(mask2);
+normxcorrAllImage = (normxcorrImage1 + normxcorrImage2 + normxcorrImage3 + normxcorrImage4) / 4;
+normxcorrAllImage = normxcorrAllImage(patternWidth : end - patternWidth, patternWidth : end - patternWidth);
+normxcorrAbsoluteImage = abs(normxcorrAllImage);
+
+mask = normxcorrAbsoluteImage < maskValue;
+se = strel('disk', diskSize);
+finalMask = imopen(mask,se);
+figure, imagesc(finalMask);
 
 image = image(5 : end - 6, 5 : end - 6);
 nextimage = image;
-nextimage(mask2) = 255;
+nextimage(finalMask)=255;
 finalimage = cat(3, nextimage, image, image);
