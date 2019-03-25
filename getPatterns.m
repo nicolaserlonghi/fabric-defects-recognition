2ae06a50ccd1e68a4e8db2c2976550cb5c467f56
Nicola Serlonghi <nicolaserlonghi@gmail.com>
2019-03-25 14:13:52 +0100

    [UPDATE] Update find patterns

--- getPatterns.m
+++ getPatterns.m
@@ -15,20 +15,30 @@ function [pattern1, pattern2, pattern3, pattern4] = getPatterns(image, startPatt
     imageWithThreshold = applyThreshold(convolvedImage, threshold);
     
     [yPositionOfOneValue, xPositionOfOneValue] = find(imageWithThreshold == 1);
+    
+    
+    positionOfXElements = find(xPositionOfOneValue < (imageSizeX - patternWidth));
+    numberOfXElements = size(positionOfXElements);
+    halfNumberOfXElements = floor(numberOfXElements(1) / 2);
+    quarterNumberOfXElements = floor(halfNumberOfXElements / 2);
+
     yPositionOfOneValueWithPatternWidth = yPositionOfOneValue + patternWidth;
     xPositionOfOneValueWithPatternWidth = xPositionOfOneValue + patternWidth;
     
     pattern1 = image(yPositionOfOneValue(1) : yPositionOfOneValueWithPatternWidth(1), xPositionOfOneValue(1) : xPositionOfOneValueWithPatternWidth(1));
-    pattern2 = image(yPositionOfOneValue(2) : yPositionOfOneValueWithPatternWidth(2), xPositionOfOneValue(2) : xPositionOfOneValueWithPatternWidth(2));
+    pattern2 = image(yPositionOfOneValue(halfNumberOfXElements) : yPositionOfOneValueWithPatternWidth(halfNumberOfXElements), xPositionOfOneValue(halfNumberOfXElements) : xPositionOfOneValueWithPatternWidth(halfNumberOfXElements));
     pattern3 = image(imageSizeY - patternWidth : imageSizeY, imageSizeX - patternWidth : imageSizeX);
-    pattern4 = image(imageSizeY - patternWidth - 1 : imageSizeY - 1 , imageSizeX - patternWidth - 1 : imageSizeX-1);
+    pattern4 = image(yPositionOfOneValue(quarterNumberOfXElements) : yPositionOfOneValueWithPatternWidth(quarterNumberOfXElements), xPositionOfOneValue(quarterNumberOfXElements) : xPositionOfOneValueWithPatternWidth(quarterNumberOfXElements));
     
     % Show patterns position
     subplot(223); imshow(imageWithThreshold);
     figure;imagesc(image); axis image; colormap gray; hold on;
     rectangle('position', [yPositionOfOneValue(1), xPositionOfOneValue(1), patternWidth, patternWidth], 'EdgeColor',[1 0 0]);
-    rectangle('position', [yPositionOfOneValue(2), xPositionOfOneValue(2), patternWidth, patternWidth], 'EdgeColor',[1 0 0]);
+    rectangle('position',[yPositionOfOneValue(halfNumberOfXElements), xPositionOfOneValue(halfNumberOfXElements), patternWidth, patternWidth], 'EdgeColor',[1 0 0]);
     rectangle('position', [imageSizeY - patternWidth, imageSizeX - patternWidth, patternWidth, patternWidth], 'EdgeColor', [1 0 0]);
-    rectangle('position', [imageSizeY - patternWidth - 1, imageSizeX - patternWidth - 1, patternWidth, patternWidth], 'EdgeColor', [1 0 0]);
+    rectangle('position',[yPositionOfOneValue(quarterNumberOfXElements), xPositionOfOneValue(quarterNumberOfXElements), patternWidth, patternWidth], 'EdgeColor',[1 0 0]);
+    
+    
+
 end
 
